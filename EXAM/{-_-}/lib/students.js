27ed0acc540e0b97376cc0498ee52523425b50a3
yamsellem <amsellem.yves@gmail.com>
2019-02-04 22:57:37 +0100

    improve {-_-} through simple setup

--- EXAM/{-_-}/lib/students.js
+++ EXAM/{-_-}/lib/students.js
@@ -2,27 +2,54 @@
 
 const fs = require('fs');
 
-module.exports = (stepNames, folder) => {
-    const path = `${folder}/`;
-    const students = {};
-
-    const studentNames = fs.readdirSync(path);
-    for (var i = 0; i < studentNames.length; i++) {
-        let stats = fs.lstatSync(path + studentNames[i])
-        if (stats.isFile())
-            continue;
-
-        students[studentNames[i]] = {score: 0, files: {}};
-
-        let exercices = fs.readdirSync(path + studentNames[i]);
-        for (var j = 0; j < exercices.length; j++) {
-            let stats = fs.lstatSync(path + studentNames[i] + '/' + exercices[j]);
-            let name = exercices[j].replace('.html', '');
-            if (stats.isDirectory() || stepNames.indexOf(name) === -1)
+module.exports = {
+    get(folder, deep) {
+        return !deep ? this.root(folder) : this.deep(folder);
+    },
+    getSteps() {
+        const steps = [];
+        this.forEach('lib/assertions', () => false, (name) => steps.push(name));
+        return steps;
+    },
+    forEach(path, excludeFn, fn) {
+        let files = fs.readdirSync(path);
+        for (let j = 0; j < files.length; j++) {
+            let stats = fs.lstatSync(path + '/' + files[j]);
+            let name = files[j].replace(/\.(html|js)/, '');
+            if (stats.isDirectory() || excludeFn(name))
                 continue;
 
-            students[studentNames[i]].files[name] = 0;
+            fn(name);
         }
+    },
+    deep(folder) {
+        const steps = this.getSteps();
+        const path = `${folder}/`;
+        const students = {};
+    
+        const studentNames = fs.readdirSync(path);
+        for (let i = 0; i < studentNames.length; i++) {
+            let stats = fs.lstatSync(path + studentNames[i])
+            if (stats.isFile())
+                continue;
+    
+            students[studentNames[i]] = {score: 0, files: {}};
+    
+            this.forEach(path + studentNames[i], (name) => steps.indexOf(name) === -1, (name) => {
+                students[studentNames[i]].files[name] = 0;
+            });
+        }
+        return students;
+    },
+    root(folder) {
+        const steps = this.getSteps();
+
+        const path = `${folder}/`;
+        const students = {'>': {score: 0, files: {}}};
+        this.forEach(path, (name) => steps.indexOf(name) === -1, (name) => {
+            students['>'].files[name] = 0;
+        });
+
+        return students;
     }
-    return students;
 }
